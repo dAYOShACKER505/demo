56e5f8a015dc02b396c5abfd68a3684af2f115e0
DevilanD <68126654+dAYOShACKER505@users.noreply.github.com>
2020-09-04 23:58:29 +0000

    Create d.py

---
+++ d.py
@@ -0,0 +1,23 @@
+#DevilanD
+#Facebook page
+
+
+
+import os
+import random
+import string
+from termcolor import colored
+
+#to clear the screen
+os.system ("clear")
+import(colored("Press enter to continue","red"))
+#to clear the screen
+os.system ("clear")
+print("\n")
+print ("\t\t  #Bomb")
+
+#logo
+
+
+import(colored("$$$$$$$$$$$$$$$$$$$$$$$$$","green")
+import(colored("*******************","red")
